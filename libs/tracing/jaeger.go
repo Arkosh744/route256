6708185f84169dbd6b7bdf0871aaf75814e2f8a0
Kotov Kirill <trukan24@gmail.com>
2023-07-08 14:31:15 +0300

    [GOHW-7]: :recycle: added tracing id log

--- libs/tracing/jaeger.go
+++ libs/tracing/jaeger.go
@@ -1,10 +1,15 @@
 package tracing
 
 import (
+	"route256/libs/log"
+
 	"github.com/uber/jaeger-client-go/config"
+	"go.uber.org/zap"
 )
 
-func Init(host, serviceName string) error{
+func Init(host, serviceName string) error {
+	log.Info("Initializing tracing", zap.String("host", host), zap.String("service", serviceName))
+
 	cfg := &config.Configuration{
 		Sampler: &config.SamplerConfig{
 			Type:  "const",
@@ -12,7 +17,6 @@ func Init(host, serviceName string) error{
 		},
 		Reporter: &config.ReporterConfig{
 			LogSpans: true,
-			// LocalAgentHostPort: "jaeger:6831",
 			LocalAgentHostPort: host,
 		},
 	}
